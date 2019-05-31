0fc927c4a6c07bef1cf044ff19500c18d0d9010f
Conrad Casper <conradcasper@icloud.com>
2019-05-31 23:06:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 # Write your code here.
 def dictionary
   convert = {
@@ -15,35 +17,13 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  # replaces long words with their expected short form
-
-    tweet.split(" ").map do |word|
-      if dictionary.keys.include?(word.downcase)
-        word = dictionary[word.downcase]
-      else
-        word
+  tweet.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
     end
   end.join(" ")
+      
+      
 end
-
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
-    puts word_substituter(phrase)
-  end
-end
-
-def selective_tweet_shortener(tweet)
-  if tweet.chars.length > 140  
-    word_substituter(tweet)
-  else 
-    tweet 
-  end 
-end 
-
-def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).chars.length > 140  
-    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
-  else 
-    word_substituter(tweet) 
-  end 
-end 
